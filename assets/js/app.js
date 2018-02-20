96bbd8f67faf356c80bbe68ec71d0bc8f6ca0ada
odelavia <odelavia@gmail.com>
2018-02-20 06:14:56 -0500

    mostly fixed hire me button issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -65,22 +65,3 @@ window.onload = function() {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #F7C331}"; //text cursor
     document.body.appendChild(css);
 };
-
-/*// navbar scroll dissappear
-$(window).scroll(function(){
-    if ($('navbar').offset().top > 50){
-        $('.navbar-fixed-top').addClass('top-nav-collapse');
-    } else {
-        $('.navbar-fixed-top').removeClass('top-nav-collapse');
-    }
-});
-
-$(function(){
-    $('.page-scroll a').bind('click', function(){
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});*/
