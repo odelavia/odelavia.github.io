aae24a4bd4e9bef4900f5acd7d6e465b990a7e1f
Oliver De La Via <odelavia@gmail.com>
2018-07-27 14:48:30 -0400

    added projects, blog and contact sections to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Hero from './Hero.js'
 import AboutContainer from './about/AboutContainer'
+import Projects from './projects/Projects.js';
+import Blog from './Blog'
+import Contact from './contact/Contact.js';
 class Home extends Component {
   render() {
     return (
       <div>
         <Hero />
         <AboutContainer />
+        <Projects />
+        <Blog />
+        <Contact />
       </div>
     );
   }
--- client/src/routing/AppRoutes.js
+++ client/src/routing/AppRoutes.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 import NavBar from '../components/layout/NavBar';
 import Home from '../components/Home';
 import Skills from '../components/Skills'
-import Projects from '../components/Projects'
+import Projects from '../components/projects/Projects'
 import Blog from '../components/Blog'
 import Contact from '../components/contact/Contact'
 import Footer from '../components/layout/Footer'
