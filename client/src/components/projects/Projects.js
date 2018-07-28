1e96ba79ef40aa1e5158a1b4da9980fc59b54d78
Oliver De La Via <odelavia@gmail.com>
2018-07-28 03:20:28 -0400

    added variety of color blends for project hover and refactored

--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -10,7 +10,7 @@ const Projects = () => (
     </div>
     <div className="projects-container">
       <div className="test-project">
-        <div className="project-description">
+        <div className="project-description-1">
           <h4>2 Bears App</h4>
           <p>Mobile point of sales app for Belgian non-profit, Mov feast</p>
           <span>
@@ -22,7 +22,7 @@ const Projects = () => (
         <div className="project-img"  style={{ backgroundImage: "url(http://www.hellothierry.com/media/01_american_ligth/82_MG_3496.jpg)" }} />
       </div>
       <div className="test-project">
-        <div className="project-description">
+        <div className="project-description-2">
           <h4>Contractor Proposal Generator</h4>
           <p>Commercial/residential renovation proposal template generator</p>
           <span>
@@ -34,7 +34,7 @@ const Projects = () => (
         <div className="project-img" style={{ backgroundImage: "url(https://image.ibb.co/mPFbmx/homepage.png)" }} />
       </div>
       <div className="test-project">
-        <div className="project-description">
+        <div className="project-description-3">
           <h4>FaceTagger</h4>
           <p>A face-tagging game for the people in your pictures that utilizes the Clarifai API</p>
           <span>
@@ -46,7 +46,7 @@ const Projects = () => (
         <div className="project-img"  style={{ backgroundImage: "url(http://www.hellothierry.com/media/01_american_ligth/58_MG_3373.jpg)" }} />
       </div>
       <div className="test-project">
-        <div className="project-description">
+        <div className="project-description-4">
           <h4>Gecho App</h4>
           <p>A video music playing app that utilizes Youtube's API</p>
           <span>
@@ -58,7 +58,7 @@ const Projects = () => (
         <div className="project-img" style={{ backgroundImage: "url(https://images.pexels.com/photos/877695/pexels-photo-877695.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)" }} />
       </div>
       <div className="test-project">
-        <div className="project-description">
+        <div className="project-description-5">
           <h4>Hangman</h4>
           <p>A modern attempt at the classic Hangman game</p>
           <span>
@@ -70,7 +70,7 @@ const Projects = () => (
         <div className="project-img"  style={{ backgroundImage: "url(http://www.hellothierry.com/media/01_american_ligth/82_MG_3496.jpg)" }} />
       </div>
       <div className="test-project">
-        <div className="project-description">
+        <div className="project-description-6">
           <h4>Glasskey</h4>
           <p>A twist on the GlassDoor App, use the app to see employee reviews</p>
           <span>
@@ -82,7 +82,7 @@ const Projects = () => (
         <div className="project-img" style={{ backgroundImage: "url(https://image.ibb.co/mPFbmx/homepage.png)" }} />
       </div>
       <div className="test-project">
-        <div className="project-description">
+        <div className="project-description-7">
           <h4>Blog App</h4>
           <p>A blog built from scratch</p>
           <span>
@@ -94,7 +94,7 @@ const Projects = () => (
         <div className="project-img"  style={{ backgroundImage: "url(http://www.hellothierry.com/media/01_american_ligth/58_MG_3373.jpg)" }} />
       </div>
       <div className="test-project">
-        <div className="project-description">
+        <div className="project-description-8">
           <h4>Note</h4>
           <p>Note-keeping app</p>
           <span>
@@ -106,7 +106,7 @@ const Projects = () => (
         <div className="project-img" style={{ backgroundImage: "url(https://images.pexels.com/photos/877695/pexels-photo-877695.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)" }} />
       </div>
       <div className="test-project">
-        <div className="project-description">
+        <div className="project-description-9">
           <h4>Todo App</h4>
           <p>Another to-do app</p>
           <span>
@@ -118,7 +118,7 @@ const Projects = () => (
         <div className="project-img"  style={{ backgroundImage: "url(http://www.hellothierry.com/media/01_american_ligth/82_MG_3496.jpg)" }} />
       </div>
       <div className="test-project">
-        <div className="project-description">
+        <div className="project-description-10">
           <h4>Auth App</h4>
           <p>An authorization app for mobile</p>
           <span>
@@ -130,7 +130,7 @@ const Projects = () => (
         <div className="project-img" style={{ backgroundImage: "url(https://image.ibb.co/mPFbmx/homepage.png)" }} />
       </div>
       <div className="test-project">
-        <div className="project-description">
+        <div className="project-description-11">
           <h4>Tech Stack</h4>
           <p>An app that shows the tech stack you know.</p>
           <span>
