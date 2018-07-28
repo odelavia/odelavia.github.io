93fe1f834a286e3547f228a0156c08e0b6ecfdc5
Oliver De La Via <odelavia@gmail.com>
2018-07-28 02:24:21 -0400

    finished setting up fully responsive projects section. Added filler imgs

--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -10,28 +10,136 @@ const Projects = () => (
     </div>
     <div className="projects-container">
       <div className="test-project">
-        <div className="project-description">TEST</div>
-        <div className="project-img" />
+        <div className="project-description">
+          <h4>2 Bears App</h4>
+          <p>Mobile point of sales app for Belgian non-profit, Mov feast</p>
+          <span>
+            <a>Live</a>
+            <span className="live-repo">//</span>
+            <a>Repo</a>
+          </span>
+        </div>
+        <div className="project-img"  style={{ backgroundImage: "url(http://www.hellothierry.com/media/01_american_ligth/82_MG_3496.jpg)" }} />
       </div>
       <div className="test-project">
-        <div className="project-description">TEST</div>
-        <div className="project-img" />
+        <div className="project-description">
+          <h4>Contractor Proposal Generator</h4>
+          <p>Commercial/residential renovation proposal template generator</p>
+          <span>
+            <a>Live</a>
+            <span className="live-repo">//</span>
+            <a>Repo</a>
+          </span>
+        </div>
+        <div className="project-img" style={{ backgroundImage: "url(https://image.ibb.co/mPFbmx/homepage.png)" }} />
       </div>
       <div className="test-project">
-        <div className="project-description">TEST</div>
-        <div className="project-img" />
+        <div className="project-description">
+          <h4>FaceTagger</h4>
+          <p>A face-tagging game for the people in your pictures that utilizes the Clarifai API</p>
+          <span>
+            <a>Live</a>
+            <span className="live-repo">//</span>
+            <a>Repo</a>
+          </span>
+        </div>
+        <div className="project-img"  style={{ backgroundImage: "url(http://www.hellothierry.com/media/01_american_ligth/58_MG_3373.jpg)" }} />
       </div>
       <div className="test-project">
-        <div className="project-description">TEST</div>
-        <div className="project-img" />
+        <div className="project-description">
+          <h4>Gecho App</h4>
+          <p>A video music playing app that utilizes Youtube's API</p>
+          <span>
+            <a>Live</a>
+            <span className="live-repo">//</span>
+            <a>Repo</a>
+          </span>
+        </div>
+        <div className="project-img" style={{ backgroundImage: "url(https://images.pexels.com/photos/877695/pexels-photo-877695.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)" }} />
       </div>
       <div className="test-project">
-        <div className="project-description">TEST</div>
-        <div className="project-img" />
+        <div className="project-description">
+          <h4>Hangman</h4>
+          <p>A modern attempt at the classic Hangman game</p>
+          <span>
+            <a>Live</a>
+            <span className="live-repo">//</span>
+            <a>Repo</a>
+          </span>
+        </div>
+        <div className="project-img"  style={{ backgroundImage: "url(http://www.hellothierry.com/media/01_american_ligth/82_MG_3496.jpg)" }} />
       </div>
       <div className="test-project">
-        <div className="project-description">TEST</div>
-        <div className="project-img" />
+        <div className="project-description">
+          <h4>Glasskey</h4>
+          <p>A twist on the GlassDoor App, use the app to see employee reviews</p>
+          <span>
+            <a>Live</a>
+            <span className="live-repo">//</span>
+            <a>Repo</a>
+          </span>
+        </div>
+        <div className="project-img" style={{ backgroundImage: "url(https://image.ibb.co/mPFbmx/homepage.png)" }} />
+      </div>
+      <div className="test-project">
+        <div className="project-description">
+          <h4>Blog App</h4>
+          <p>A blog built from scratch</p>
+          <span>
+            <a>Live</a>
+            <span className="live-repo">//</span>
+            <a>Repo</a>
+          </span>
+        </div>
+        <div className="project-img"  style={{ backgroundImage: "url(http://www.hellothierry.com/media/01_american_ligth/58_MG_3373.jpg)" }} />
+      </div>
+      <div className="test-project">
+        <div className="project-description">
+          <h4>Note</h4>
+          <p>Note-keeping app</p>
+          <span>
+            <a>Live</a>
+            <span className="live-repo">//</span>
+            <a>Repo</a>
+          </span>
+        </div>
+        <div className="project-img" style={{ backgroundImage: "url(https://images.pexels.com/photos/877695/pexels-photo-877695.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)" }} />
+      </div>
+      <div className="test-project">
+        <div className="project-description">
+          <h4>Todo App</h4>
+          <p>Another to-do app</p>
+          <span>
+            <a>Live</a>
+            <span className="live-repo">//</span>
+            <a>Repo</a>
+          </span>
+        </div>
+        <div className="project-img"  style={{ backgroundImage: "url(http://www.hellothierry.com/media/01_american_ligth/82_MG_3496.jpg)" }} />
+      </div>
+      <div className="test-project">
+        <div className="project-description">
+          <h4>Auth App</h4>
+          <p>An authorization app for mobile</p>
+          <span>
+            <a>Live</a>
+            <span className="live-repo">//</span>
+            <a>Repo</a>
+          </span>
+        </div>
+        <div className="project-img" style={{ backgroundImage: "url(https://image.ibb.co/mPFbmx/homepage.png)" }} />
+      </div>
+      <div className="test-project">
+        <div className="project-description">
+          <h4>Tech Stack</h4>
+          <p>An app that shows the tech stack you know.</p>
+          <span>
+            <a>Live</a>
+            <span className="live-repo">//</span>
+            <a>Repo</a>
+          </span>
+        </div>
+        <div className="project-img"  style={{ backgroundImage: "url(http://www.hellothierry.com/media/01_american_ligth/58_MG_3373.jpg)" }} />
       </div>
     </div>
   </div>
