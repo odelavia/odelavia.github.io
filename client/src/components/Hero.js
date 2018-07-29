1fe19f62526e33127a7672cb2506558fbcf8e3d3
odelavia <odelavia@gmail.com>
2018-07-29 14:12:31 -0400

    optimized images

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import heroImage from '../assets/img/splash2.png'
+import heroImage from '../assets/img/splash.png'
 
 const Hero = () => (
   <div className="section" id="top">
