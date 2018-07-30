be3c1118fc28c2b38227184c80ecc0b073b5dd10
odelavia <odelavia@gmail.com>
2018-07-30 11:51:42 -0400

    set up simpler and more efficient hero.

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -6,20 +6,18 @@ const Hero = () => (
   <div className="hero">
     <div className="container full">
       <div className="content">
-        <div className="hero-info">
-          <h1 className="tagline">Passionate about implementing creative solutions for the digital world.</h1>
-          <ul className="social">
-            <li>
-              <a href="https://github.com/odelavia" className="special" target="_blank" rel="noopener">github</a>
-            </li>
-            <li>
-              <a href="https://twitter.com/odelavia" className="special" target="_blank" rel="noopener">twitter</a>
-            </li>
-          </ul>
+        <div className="splash-image">
+          <img src={heroImage} alt="portrait" />
         </div>
-      </div>
-      <div className="splash-image">
-        <img src={heroImage} alt="portrait" />
+        <h1 className="tagline">Passionate about implementing creative solutions for the digital world.</h1>
+        <ul className="social">
+          <li>
+            <a href="https://github.com/odelavia" className="special" target="_blank" rel="noopener">github</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/odelavia" className="special" target="_blank" rel="noopener">twitter</a>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
