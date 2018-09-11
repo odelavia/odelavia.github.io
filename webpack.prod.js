e3f67520aaf66209e6fb934dbd45ef343b517c64
odelavia <odelavia@gmail.com>
2018-09-11 16:17:52 -0400

    added Optimize CssAssetsPlugin to webpack to minify all css files.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin'); // compress/minify js
 const ExtractTextPlugin = require('extract-text-webpack-plugin'); // create separate css file for production
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'); // minifies css files
 const common = require('./build-utils/webpack.common.js');
 
 module.exports = merge(common, {
@@ -29,6 +30,14 @@ module.exports = merge(common, {
       filename: 'style.[hash:4].css',
       disable: false,
       allChunks: true,
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.optimize\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+      canPrint: true
     }),
     new UglifyJSPlugin({
       sourceMap: true,
