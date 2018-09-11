639b0faf7df5dcf696c8c3540b97aa1fdf984e49
odelavia <odelavia@gmail.com>
2018-09-11 16:10:02 -0400

    set options in css loader to minimize single css files

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,7 +11,15 @@ module.exports = merge(common, {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader'],
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true
+              }
+            },
+            'sass-loader'
+          ],
         }),
       },
     ],
